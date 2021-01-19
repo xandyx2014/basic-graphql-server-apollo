4efcc74eb412a6deea5bba01c8f225a9769dacad
xandyx2014 <xandyx2014@gmail.com>
2021-01-19 14:39:37 -0400

    progando package.json

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,6 +3,7 @@ import 'graphql-import-node';
 import { makeExecutableSchema } from "graphql-tools";
 import * as typeDefs from './schema.graphql';
 import resolvers from '../resolvers/resolversMap';
+
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
     resolvers
